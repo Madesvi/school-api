e9e18fd0c5cf49c475fd5b5f6fe7116fb4e12c12
Madesvi <msvbui@gmail.com>
2026-03-02 13:25:25 +0300

    refactor: backup add

--- internal/models/teacher.go
+++ internal/models/teacher.go
@@ -1,4 +1,7 @@
+// Package models use for define models for db
 package models
+
+import "time"
 
 // type Teacher struct {
 // 	ID        int    `json:"id,omitempty"`         // 8 bytes
@@ -10,10 +13,12 @@ package models
 // }
 
 type Teacher struct {
-	ID        int    `gorm:"primaryKey" json:"id,omitempty"`
-	FirstName string `gorm:"not null" json:"first_name,omitempty"`
-	LastName  string `gorm:"not null" json:"last_name,omitempty"`
-	Email     string `gorm:"uniqueIndex;not null" json:"email,omitempty"`
-	Class     string `gorm:"not null" json:"class,omitempty"`
-	Subject   string `gorm:"not null" json:"subject,omitempty"`
+	ID        int       `gorm:"primaryKey" json:"id,omitempty"`
+	FirstName string    `gorm:"not null" json:"first_name,omitempty"`
+	LastName  string    `gorm:"not null" json:"last_name,omitempty"`
+	Email     string    `gorm:"uniqueIndex;not null" json:"email,omitempty"`
+	Class     string    `gorm:"not null" json:"class,omitempty"`
+	Subject   string    `gorm:"not null" json:"subject,omitempty"`
+	CreatedAt time.Time `json:"created"`
+	UpdatedAt time.Time `json:"updated"`
 }
