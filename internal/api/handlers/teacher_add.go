fa2e1f9658cd31899a89dda81a7436185790792f
Madesvi <msvbui@gmail.com>
2026-03-09 16:29:56 +0300

    fear(api): add data validation

--- internal/api/handlers/teacher_add.go
+++ internal/api/handlers/teacher_add.go
@@ -3,7 +3,10 @@ package handlers
 import (
 	"context"
 	"encoding/json"
+	"io"
 	"net/http"
+	"reflect"
+	"strings"
 
 	"rest-api-app/internal/models"
 
@@ -14,13 +17,76 @@ type AddTeacher interface {
 	AddTeacher(ctx context.Context, newTeachers []models.Teacher) ([]models.Teacher, error)
 }
 
+func GetFieldNames(model any) []string {
+	val := reflect.TypeOf(model)
+	fields := []string{}
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		fieldToAdd := strings.TrimSuffix(field.Tag.Get("json"), ",omitempty")
+		fields = append(fields, fieldToAdd) // Get JSON tag
+	}
+	return fields
+}
+
 func AddTeacherHandler(add AddTeacher) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var newTeachers []models.Teacher
-		err := json.NewDecoder(r.Body).Decode(&newTeachers)
+		var rawTeachers []map[string]interface{}
+
+		// WE JUST READ THE BODY ONCE - BYTE SLICE AND USE body for Unmarshal for more times
+		// When we use r.Body more than once after first read our body will be empty
+
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Error reading request body", http.StatusInternalServerError)
+			return
+		}
+		defer r.Body.Close()
+
+		err = json.Unmarshal(body, &rawTeachers)
 		if err != nil {
 			http.Error(w, "Invalid request Body", http.StatusBadRequest)
 			return
+		}
+
+		fields := GetFieldNames(models.Teacher{})
+
+		allowedFields := make(map[string]struct{})
+		for _, field := range fields {
+			allowedFields[field] = struct{}{}
+		}
+
+		for _, teacher := range rawTeachers {
+			for key := range teacher {
+				_, ok := allowedFields[key]
+				if !ok {
+					http.Error(w, "Unacceptable field found in request. Only use allowed fields", http.StatusBadRequest)
+					return
+				}
+			}
+		}
+
+		err = json.Unmarshal(body, &newTeachers)
+		if err != nil {
+			http.Error(w, "Invalid request Body", http.StatusBadRequest)
+			return
+		}
+
+		for _, teacher := range newTeachers {
+			// if teacher.FirstName == "" || teacher.LastName == "" || teacher.Email == "" || teacher.Class == "" || teacher.Subject == "" {
+			// 	http.Error(w, "All field are required", http.StatusBadRequest)
+			// 	return
+			// }
+
+			val := reflect.Indirect(reflect.ValueOf(teacher))
+			for i := 0; i < val.NumField(); i++ {
+				field := val.Field(i)
+				if field.Kind() == reflect.String && field.String() == "" {
+					http.Error(w, "All field are required", http.StatusBadRequest)
+					return
+				}
+			}
 		}
 
 		newTeachers, err = add.AddTeacher(r.Context(), newTeachers)
--- internal/models/teacher.go
+++ internal/models/teacher.go
@@ -13,12 +13,12 @@ import "time"
 // }
 
 type Teacher struct {
-	ID        int       `gorm:"primaryKey" json:"id,omitempty"`
-	FirstName string    `gorm:"not null" json:"first_name,omitempty"`
-	LastName  string    `gorm:"not null" json:"last_name,omitempty"`
-	Email     string    `gorm:"uniqueIndex;not null" json:"email,omitempty"`
-	Class     string    `gorm:"not null" json:"class,omitempty"`
-	Subject   string    `gorm:"not null" json:"subject,omitempty"`
-	CreatedAt time.Time `json:"created"`
-	UpdatedAt time.Time `json:"updated"`
+	ID        int    `gorm:"primaryKey" json:"id,omitempty"`
+	FirstName string `gorm:"not null" json:"first_name,omitempty"`
+	LastName  string `gorm:"not null" json:"last_name,omitempty"`
+	Email     string `gorm:"uniqueIndex;not null" json:"email,omitempty"`
+	Class     string `gorm:"not null" json:"class,omitempty"`
+	Subject   string `gorm:"not null" json:"subject,omitempty"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
