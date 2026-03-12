c7e986d299b7abebd37e7739458cf68e4d6339a5
Madesvi <msvbui@gmail.com>
2026-03-12 13:15:47 +0300

    refactor: restore postgre connect

---
+++ internal/repositories/postgre/postgreconnect.go
@@ -0,0 +1,41 @@
+// Package postgre provide connection to DB
+package postgre
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/rs/zerolog/log"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectDB() (*gorm.DB, error) {
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbname, port)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		AllowGlobalUpdate: false, // Not allow delete or update all rows withot Where
+	})
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to connect to db")
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, err
+	}
+
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
+	log.Info().Msg("Connect to PostgreSQL DB")
+
+	return db, nil
+}
